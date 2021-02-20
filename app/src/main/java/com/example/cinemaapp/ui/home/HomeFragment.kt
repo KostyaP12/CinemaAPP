3134497a4168bac728edbf562393bab7004c35a9
Petrov <kostyapetrov12rus@gmail.com>
2021-02-20 09:58:59 +0300

    hw

--- app/src/main/java/com/example/cinemaapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cinemaapp/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.cinemaapp.ui.home
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,17 +10,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cinemaapp.R
 import com.example.cinemaapp.model.AppState
+import com.example.cinemaapp.model.MyBroadcastReceiver
 import com.example.cinemaapp.model.OnItemPreviewClickListener
 import com.example.cinemaapp.model.OriginalSourcePreview
 import com.example.cinemaapp.ui.original_source_preview.OriginalSourcePreviewFragment
 import com.example.cinemaapp.ui.recycler_view.VerticalAdapter
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+    private var snackBar: Snackbar? = null
     private lateinit var homeViewModel: HomeViewModel
     private val adapter = VerticalAdapter(object : OnItemPreviewClickListener {
         override fun onItemPreviewClickListener(originalSourcePreview: OriginalSourcePreview) {
@@ -70,6 +77,31 @@ class HomeFragment : Fragment() {
                     .setAction(getString(R.string.reload)) { homeViewModel.getDataFromLocalSource() }
                     .show()
             }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        context?.registerReceiver(MyBroadcastReceiver(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    fun onNetworkConnectionChanged(isConnected: Boolean) {
+        showNetworkMessage(isConnected)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        MyBroadcastReceiver.connectivityReceiverListener = MyBroadcastReceiver.connectivityReceiverListener
+    }
+
+    private fun showNetworkMessage(isConnected: Boolean) {
+        if (!isConnected) {
+            snackBar = Snackbar.make(requireView().findViewById(R.id.homeView), "You are offline", Snackbar.LENGTH_LONG)
+            snackBar?.duration = BaseTransientBottomBar.LENGTH_INDEFINITE
+            snackBar?.show()
+        } else {
+            snackBar?.dismiss()
         }
     }
 }
